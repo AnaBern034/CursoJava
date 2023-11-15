22a3b99bec924d7dc77a1855e18a59d747960172
ana <ana.bernardes@zup.com.br>
2023-11-15 16:44:47 -0300

    Aula sobre conversão usando cast e desafios e operadores

--- AulaDoYt/src/main/java/org/example/Ternario.java
+++ AulaDoYt/src/main/java/org/example/Ternario.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Ternario {
     public static void main(String[] args) {
-        Double nota = 4.8;
+        double nota = 4.8;
 
-        String média = nota >= 5.0 ? "aprovado" : "reprovado";
+        String media = nota >= 5.0 ? "approval" : "preapproval";
         /* ? é o operador ternario e com ele é acompanhado por dois simbolos ,
         um deles é comparando a resposta anterior (?) e o outro operador é os (:) que simboliza
         for não um resultado é outro, como se fosse um else-if
         */
-        System.out.println(média);
+        System.out.println(media);
     }
 }
