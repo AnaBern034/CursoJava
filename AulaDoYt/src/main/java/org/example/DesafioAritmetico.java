45a8a406756c3e156e6e8333b9d6db98a7cbe8ed
ana <ana.bernardes@zup.com.br>
2023-11-15 16:37:12 -0300

    Aula sobre conversão usando cast e desafios

--- AulaDoYt/src/main/java/org/example/DesafioAritmetico.java
+++ AulaDoYt/src/main/java/org/example/DesafioAritmetico.java
@@ -2,31 +2,23 @@ package org.example;
 
 public class DesafioAritmetico {
     public static void main(String[] args) {
-        Integer nm2 = 6 * (3+2) ;
 
-        Integer result1 = (int) Math.pow(nm2,2);
+        Integer result1 = (int) Math.pow(6 * (3+2),2);
         Integer result2 = 3*2;
         Integer divisão = result1/result2;
 
         System.out.printf("Primeiro resultado : %d ",divisão);
 
-
-        Integer nm3 =(1-5) * (2-7);
-        Integer nm4 = nm3/2;
-        Integer result3 = (int) Math.pow(nm4,2);
-
+        Integer result3 = (int) Math.pow( (double) (1 - 5) * (2 - 7) / 2,2);
         System.out.printf("\nSegundo resultado %d ",result3);
 
-        Integer resultadoQuase = divisão - result3;
-        System.out.printf("\nTerceiro resultado %d",resultadoQuase);
+        System.out.printf("\nTerceiro resultado %d", divisão - result3);
 
-        resultadoQuase = (int) Math.pow(resultadoQuase,3);
+        Integer resultadoQuase= (int) Math.pow(divisão-result3,3);
         Integer resultadoDeBaixo = (int) Math.pow(10,3); // fazendo a conversão para int
 
         Integer resultadoFinal = resultadoQuase / resultadoDeBaixo;
-       System.out.printf("\nResultado final %d",resultadoFinal);
-
-
+          System.out.printf("\nResultado final %d",resultadoFinal);
 
     }
 }
